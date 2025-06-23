1249ac66ee4f2cf607351095cdee7b03bc390eac
Ranvijay <ranvijaychouhan12@gmail.com>
2025-06-23 14:29:40 +0530

    Added some configs to Repo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
 
+let baseUrl;
+
+switch (process.env.NEXT_PUBLIC_STAGE?.trim()) {
+  case "DEV":
+    baseUrl = "https://google.com/";
+    break;
+  case "TEST":
+    baseUrl = "https://google.com/";
+    break;
+  case "PROD":
+    baseUrl = "hhttps://google.com/";
+    break;
+  default:
+    baseUrl = "https://google.com/";
+    break;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    baseUrl,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
